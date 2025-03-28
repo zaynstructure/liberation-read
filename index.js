b87d3b2ce6677ef371d3a4071dfc74a45f08eab3
zaynstructure <zayn.wiwchar@gmail.com>
2025-03-28 13:27:14 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+// liberatory-map-starter/src/index.js
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
